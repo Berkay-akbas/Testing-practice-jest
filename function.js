9e7166e12f1e876a04182933b4fdfeaf8a434ccc
Berkay Akbas <berkayakbas@gmail.com>
2022-07-12 23:52:42 +0300

    Functions with tests

--- function.js
+++ function.js
@@ -1,35 +1,35 @@
 
-function stringLength(str) {
-  if(str.split("").length < 1 || str.split("").length > 10) {
-    throw new Error('not met');
-  }
-  return str.split("").length;
-}
+// function stringLength(str) {
+//   if(str.split("").length < 1 || str.split("").length > 10) {
+//     throw new Error('not met');
+//   }
+//   return str.split("").length;
+// }
 
 
-const reverseString = (str) => {
-  return str.split('').reverse().join('');
-}
+// const reverseString = (str) => {
+//   return str.split('').reverse().join('');
+// }
 
-class Calculator {
-  add(a, b) {
-    return a + b;
-  }
-  substract(a, b) {
-    return a - b;
-  }
-  divide(a, b) {
-    return a / b;
-  }
-  multiply(a, b) {
-    return a * b;
-  }
-}
+// class Calculator {
+//   add(a, b) {
+//     return a + b;
+//   }
+//   substract(a, b) {
+//     return a - b;
+//   }
+//   divide(a, b) {
+//     return a / b;
+//   }
+//   multiply(a, b) {
+//     return a * b;
+//   }
+// }
 
-let myCalculator = new Calculator();
+// let myCalculator = new Calculator();
 
-const capitalize = (str) => {
- return str.charAt(0).toUpperCase() + str.slice(1);
-}
+// const capitalize = (str) => {
+//  return str.charAt(0).toUpperCase() + str.slice(1);
+// }
 
-module.exports = { stringLength, reverseString, myCalculator, capitalize };
+// module.exports = { stringLength, reverseString, myCalculator, capitalize };
